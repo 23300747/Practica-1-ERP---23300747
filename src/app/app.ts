612e02996200b396064ea4a39e33a7193cbc5673
Calci514 <alonso.j.g2008@gmail.com>
2026-09-05 21:01:15 -0600

    Primer committ: Desarrollo del frontend

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Prac1ERP');
+  mostrarLayout = signal(false);
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.mostrarLayout.set(this.router.url !== '/');
+    });
+  }
+
+  cerrarSesion(): void {
+    this.router.navigate(['/']);
+  }
 }
